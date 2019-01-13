589e2d955f21b8d762e26c3ea16a40d763b829f2
chrisjung18 <ntsachoung@gmail.com>
2019-01-13 23:45:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<head>
+	<title>Portfolio Partable</title>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="Css/font-awesome.min.css">
+	<link rel="stylesheet" href="Css/bootstrap.css">
+	<link rel="stylesheet" href="Css/style.css" />
+</head>
+<body>
+	<!-- header -->
+	<header class="container-fluid header">
+		<div class="container">
+			<a href="#" class="logo">Mon Portfolio</a>
+			<nav class="menu">
+				<a href="#">Accueil</a>
+				<a href="#about">A Propos</a>
+				<a href="#portfolio">Portfolio</a>
+				<a href="#contact">Contact</a>
+			</nav>
+		</div>
+	</header>
+	<!-- end header -->
+
+	<!-- banner -->
+	<section class="container-fluid banner">
+		<div class="ban">
+			<img src="img/banner.jpg" alt="bannière du site" />
+		</div>
+		<div class="inner-banner">
+				<h1>Bienvenue sur mon portfolio !</h1>
+				<a href="#contact"><button class="btn btn-custom">Contactez-moi!</button></a>
+		</div>
+	</section>
+	<!-- end banner -->
+
+	<!-- about -->
+	<section class="container-fluid about">
+		<div class="container">
+			<div class="row">
+				<div class="container-fluid">
+					<h2 id="about"> A propos de moi</h2>
+					<hr class="separator">
+				</div>
+				<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					<h2>Etudes</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor 
+					</p>
+				</article>
+				<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					<h2>Expériences</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor 
+					</p>
+				</article>
+				<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					<h2>Rêves</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor 
+					</p>
+				</article>
+				<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					<h2>Sports</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor 
+					</p>
+				</article>
+				<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					<h2>Musiques</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor 
+					</p>
+				</article>
+				<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					<h2>Hobbies</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor 
+					</p>
+				</article>
+			</div>
+		</div>
+	</section>
+	<!-- end about -->
+
+	<!-- portfolio -->
+	<section class="container-fluid portfolio">
+		<div class="container">
+			<div class="container-fluid">
+				<h2 id="portfolio" style="color:#2C3C50">Mon portfolio</h2>
+				<hr class="separator">
+			</div>	
+			<div class="container-fluid">
+				<div class="row gal">
+					<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12 item-folio">
+						<a href="img/gallery/photo1.jpg"><img src="img/gallery/photo1.jpg" class="f-img img-fluid mx-auto" /></a>
+					</article>
+					<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12 item-folio">
+						<a href="img/gallery/photo2.jpg"><img src="img/gallery/photo2.jpg" class="f-img img-fluid mx-auto" /></a>
+					</article>
+					<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12 item-folio">
+						<a href="img/gallery/photo4.jpg"><img src="img/gallery/photo4.jpg" class="f-img img-fluid mx-auto" /></a>
+					</article>
+					<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12 item-folio">
+						<a href="img/gallery/photo5.jpg"><img src="img/gallery/photo5.jpg" class="f-img img-fluid mx-auto" /></a>
+					</article>
+					<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12 item-folio">
+						<a href="img/gallery/photo6.jpg"><img src="img/gallery/photo6.jpg" class="f-img img-fluid mx-auto" /></a>
+					</article>
+					<article class="col-md-4 col-lg-4 col-xs-12 col-sm-12 item-folio">
+						<a href="img/gallery/photo7.jpg"><img src="img/gallery/photo7.jpg" class="f-img img-fluid mx-auto" /></a>
+					</article>
+					<div class="container-fluid">
+					<div class="btn-plus">
+						<a href="#">
+							<button class="btn btn-custom">Plus de photos</button>
+						</a>
+					</div>
+			</div>
+		</div>
+	</section>
+	<!-- end portfolio -->
+
+	<!-- footer / contact -->
+	<footer class="container-fluid footer">
+		<div class="container">
+			<div class="container-fluid">
+				<h2 id="contact">Contactez moi !</h2>
+				<hr class="separator">
+			</div>	
+			<div class="row contain-form">
+				<form method="POST" action="" class="contact-form">
+					<input type="text" name="name" placeholder="Entrez votre nom" onfocus="this.placeholder=''" onblur="this.placeholder='Entrez votre nom'" required="required" />
+					<input type="email" name="email" placeholder="Entrez votre addresse mail" onfocus="this.placeholder=''" onblur="this.placeholder='Entrez votre addresse mail'" required="required" />
+					<div>
+						<textarea class="" placeholder="Votre message !" onfocus="this.placeholder=''" onblur="this.placeholder='Votre message !'" required="required" /></textarea>
+					</div>
+					<div>
+						<button class="btn btn-custom">Enoyer!</button>
+					</div>
+					<hr class="final">
+					<p>© Copyright Chris Jung's(Ghost) Portfolio 2018. All Rights Reserved !</p>
+					<p class="details">Webmaster : Chris-Jung<br>
+					jungscreation@gmail.com</p>
+				</form>
+			</div>
+		</div>
+	</footer>
+	<!-- end footer -->
+</body>
+</html>
